ab260893c541e0d919eb7f7fe670b4486eabd58e
AlexanderUcun <jmueses@ucundinamarca.edu.co>
2026-09-19 01:22:12 -0500

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/Mi_Malla/sw.js', { scope: '/Mi_Malla/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('./sw.js', { scope: './' })})}
